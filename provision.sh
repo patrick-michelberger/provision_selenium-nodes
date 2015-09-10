3b833b2071c1a80b973d0b7129ea00ef4c778344
patrick_michelberger <pmichelberger@gmx.de>
2015-09-10 19:59:18 +0200

    ntp time sync added & container iteration removed

--- provision.sh
+++ provision.sh
@@ -5,12 +5,8 @@ sudo dpkg-reconfigure -f noninteractive tzdata
 
 sudo apt-get update -y
 sudo apt-get install -y language-pack-de
+sudo apt-get install -y ntp
 
 PUBLIC_IP=$(wget http://ipecho.net/plain -O - -q)
 
-array=( 1 2 3 4 )
-for i in "${array[@]}"
-do
-	sudo docker run -d -p 5555:5555 -e REMOTE_HOST="http://$PUBLIC_IP:5555" -e HUB_PORT_4444_TCP_ADDR="54.93.73.118" -e HUB_PORT_4444_TCP_PORT="4444" --name "chrome-node-$i" selenium/node-chrome:2.47.1
-done
-
+sudo docker run -d -p 5555:5555 -e REMOTE_HOST="http://$PUBLIC_IP:5555" -e HUB_PORT_4444_TCP_ADDR="54.93.112.191" -e HUB_PORT_4444_TCP_PORT="4444" --name "chrome-node" oldjoe/node-chrome
